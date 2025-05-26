50e169a0883d2c136a9b77658c7bf9e864578634
Vincenteous William <vincenteous.william@global.ntt>
2025-05-27 00:21:10 +0700

    Change starter prompts showed after opening the Chat page based on Query Mode

--- app/SharedWebComponents/Components/Examples.razor.cs
+++ app/SharedWebComponents/Components/Examples.razor.cs
@@ -6,10 +6,15 @@ public sealed partial class Examples
 {
     [Parameter, EditorRequired] public required string Message { get; set; }
     [Parameter, EditorRequired] public EventCallback<string> OnExampleClicked { get; set; }
+    [CascadingParameter(Name = "Settings")]
+    public RequestSettingsOverrides? Settings { get; set; }
 
-    private string WhatIsIncluded { get; } = "What is included in my Northwind Health Plus plan that is not in standard?";
-    private string WhatIsPerfReview { get; } = "What happens in a performance review?";
-    private string WhatDoesPmDo { get; } = "What does a Product Manager do?";
+    private string DocStarterQuery1 { get; } = "What is NTT Code of Conduct?";
+    private string DocStarterQuery2 { get; } = "Can my annual leave credit be carried forward into the next year?";
+    private string DocStarterQuery3 { get; } = "What is the benefit of Workday system?";
+    private string DBStarterQuery1 { get; } = "Show me top 5 overdue invoices";
+    private string DBStarterQuery2 { get; } = "What is the outstanding balance for Baker-Myers?";
+    private string DBStarterQuery3 { get; } = "Count payments by payment method for invoices issued in 2024";
 
     private async Task OnClickedAsync(string exampleText)
     {
